2436bc039b3c6cf81bc5edaf1f7e454e4d4aab18
MosheBakshi <moshetrojan@gmail.com>
2020-11-17 20:17:51 +0200

    "DB FIX - POSTMAN SETTING SHOULD BE ON JSON MODE

--- Matak-app/Matak-server/models/path-model.js
+++ Matak-app/Matak-server/models/path-model.js
@@ -10,7 +10,7 @@ const Path = new Schema(
         // path_id: { type: Number, required: true },
         // path_from: { type: JSONGEO, required: true },
         // path_to: { type: JSONGEO, required: true },
-        // terms_text: { type: String, required: true },
+        terms_text: { type: String, required: true },
         // approval_user_id: { type: Number, required: true },
     },
     { timestamps: true },
