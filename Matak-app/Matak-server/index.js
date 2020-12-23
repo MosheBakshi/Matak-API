2e151576cff7490775703a4d22729955e86fdc58
MosheBakshi <moshetrojan@gmail.com>
2020-12-23 11:44:24 +0200

    merging all into the workflow

--- Matak-app/Matak-server/index.js
+++ Matak-app/Matak-server/index.js
@@ -33,10 +33,7 @@ app.use('/api', organizationRouter)
 app.use('/api', statusRouter)
 app.use('/api', carRouter)
 app.use('/api', notificationRouter)
-//
-// Updated from workflow to moshe
+
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
 
-// updated for aviel's
-
-// updated for or's
+//update of workflow to share every one with oldest version
