13172934fbcecf50189d5a3ef66776dc131cdee5
MosheBakshi <moshetrojan@gmail.com>
2020-12-23 11:41:58 +0200

    all routes using protected routing for middleware option

--- Matak-app/Matak-server/routes/status-router.js
+++ Matak-app/Matak-server/routes/status-router.js
@@ -1,13 +1,15 @@
 const express = require('express')
-
+const { withJWTAuthMiddleware } = require("express-kun");
 const StatusCtrl = require('../controllers/status-ctrl')
 
 const router = express.Router()
+const protectedRouter = withJWTAuthMiddleware(router, "Cvbs!#56drsg575jrfsd@23456ewdg1");
 
-router.post('/Status', StatusCtrl.postStatuses)
+
+protectedRouter.post('/Status', StatusCtrl.postStatuses)
 // router.get('/path/:Name', StatusCtrl.getStatusByName)
-router.get('/Statuses', StatusCtrl.getStatuses)
-router.post('/StatusByName', StatusCtrl.getStatusByName)
+protectedRouter.get('/Statuses', StatusCtrl.getStatuses)
+protectedRouter.post('/StatusByName', StatusCtrl.getStatusByName)
 
 
 module.exports = router
--- Matak-app/Matak-server/routes/user-router.js
+++ Matak-app/Matak-server/routes/user-router.js
@@ -7,12 +7,12 @@ const router = express.Router()
 const protectedRouter = withJWTAuthMiddleware(router, "Cvbs!#56drsg575jrfsd@23456ewdg1");
 
 // POSTS
-router.post('/user', UserCtrl.createUser)
-router.post('/user/login',UserCtrl.loginUser)
-router.post('/user/mobile',UserCtrl.getUserByMobile)
-router.post('/user/email',UserCtrl.getUserByEmail)
-router.post('/user/first_name',UserCtrl.getUserByFirstName)
-router.post('/user/organ',UserCtrl.getAllUsersByOrganName)
+protectedRouter.post('/user', UserCtrl.createUser)
+protectedRouter.post('/user/login',UserCtrl.loginUser)
+protectedRouter.post('/user/mobile',UserCtrl.getUserByMobile)
+protectedRouter.post('/user/email',UserCtrl.getUserByEmail)
+protectedRouter.post('/user/first_name',UserCtrl.getUserByFirstName)
+protectedRouter.post('/user/organ',UserCtrl.getAllUsersByOrganName)
 
 // GETS
 protectedRouter.get('/users', UserCtrl.getAllUsers)
