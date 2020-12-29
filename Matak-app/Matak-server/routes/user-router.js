37accab33ffe48ad21a616e4e03236a6a6d841fe
OrAmano <oramano5@gmail.com>
2020-12-29 20:14:23 +0200

    over

--- Matak-app/Matak-server/routes/user-router.js
+++ Matak-app/Matak-server/routes/user-router.js
@@ -10,16 +10,6 @@ const protectedRouter = withJWTAuthMiddleware(router, "Cvbs!#56drsg575jrfsd@2345
 // POSTS
 router.post('/user', UserCtrl.createUser)
 router.post('/user/login',UserCtrl.loginUser)
-<<<<<<< HEAD
-router.post('/user/mobile',UserCtrl.getUserByMobile)
-router.post('/user/email',UserCtrl.getUserByEmail)
-router.post('/user/first_name',UserCtrl.getUserByFirstName)
-router.post('/user/organ',UserCtrl.getAllUsersByOrganName)
-
-// GETS
-protectedRouter.get('/users', UserCtrl.getAllUsers)
-protectedRouter.get('/user/:id', UserCtrl.getUserById)
-=======
 protectedRouter.post('/user/mobile',UserCtrl.getUserByMobile)
 protectedRouter.post('/user/email',UserCtrl.getUserByEmail)
 protectedRouter.post('/user/first_name',UserCtrl.getUserByFirstName)
@@ -28,7 +18,6 @@ router.post('/user/organ', OrganCtrl.checkOrganName, UserCtrl.getAllUsersByOrgan
 // GETS
 protectedRouter.get('/users', UserCtrl.getAllUsers)
 protectedRouter.get('/user/:id', UserCtrl.getUserById)//need to be fixed
->>>>>>> 17c8b0d004f2bd21f9228a80f59caccd6f17adf2
 
 
 module.exports = router
