0889105ddc1e7b5c938927477e104ef940c0b96c
MosheBakshi <moshetrojan@gmail.com>
2021-03-03 16:37:40 +0200

    'Took last ver'

--- Matak-app/Matak-server/models/path-model.js
+++ Matak-app/Matak-server/models/path-model.js
@@ -9,7 +9,7 @@ const Schema = mongoose.Schema
 
 const Path = new Schema(
 {
-      Array_Of_Points: { type: GeoJSON, required: true}, // not done, need to be decided by the group how to parse the data
+      // Array_Of_Points: { type: GeoJSON, required: true}, // not done, need to be decided by the group how to parse the data
       // Terms_Text: { type: String, required: true },
       // Approval_User_Id: { type: String, required: false , default: "None"},
       // Path_Name: { type: String, required: true },
@@ -17,7 +17,7 @@ const Path = new Schema(
       // Start_Date: { type: Date, required: true},
       // End_Date: { type: Date, required: true},
       // Reason_Text: { type: String, required: true},
-      // Involved_Organ_Array: {type: [String], required: true, default: "None"},
+      Involved_Organ_Array: {type: [String], required: true, default: "None"},
       // Escort_Organ_Array: {type: [String], required: true, default: "None"},
       // Vehicles_Id_Involved_Array: { type: [String], required: false, default: "On foot"},
       // Status_Name: {type: String, required: true, default: "Submitted"},
