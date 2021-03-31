829cc3b4aebd77f768cd2ec7f05c05fca2d0b6ef
MosheBakshi <moshetrojan@gmail.com>
2021-03-31 14:46:09 +0300

    eyals env edition added to workflow

--- Matak-app/Matak-server/index.js
+++ Matak-app/Matak-server/index.js
@@ -43,5 +43,5 @@ app.use('/api', notificationRouter)
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
 
-//latest version of main pulled to workflow
+//latest version of main pulled to workflow with env edition
 
