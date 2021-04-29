84bc11187e8a669a62c7a3daa92bf8ff03dd8261
aviel080 <aviel080@gmail.com>
2021-04-29 19:26:38 +0300

    merge

--- Matak-app/Matak-server/routes/notification-router.js
+++ Matak-app/Matak-server/routes/notification-router.js
@@ -6,13 +6,9 @@ const Permissions = require('../middleware/permissions')
 const Validations = require('../middleware/validation')
 
 /* CRUD */
-<<<<<<< HEAD
 router.get('/notification',Validations.verifyUser, Permissions.GetNotificationPermission, NotificationCtrl.getNotificationBy)
-=======
 router.put('/notification/read',Validations.verifyUser, NotificationCtrl.updateRead)
 router.get('/notification/get/len',Validations.verifyUser, Permissions.GetUnreadLen, NotificationCtrl.getNotificationLen)
-router.get('/notification/get',Validations.verifyUser, Permissions.GetNotificationPermission, NotificationCtrl.getNotificationBy)
->>>>>>> 265fee861c395fba013808b042f5465771c39388
 router.post('/notification',Validations.verifyUser, NotificationCtrl.createNotification)
 router.post('/notification/get',Validations.verifyUser, NotificationCtrl.getNotificationBy)
 router.put('/notification',Validations.verifyUser, NotificationCtrl.updateNotification)
